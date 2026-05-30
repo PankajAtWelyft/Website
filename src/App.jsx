6926a01548a1adc8da6e0c7be0f9c2447065bf0f
shyamsunder98929 <shyamsuthar9829>
2026-05-30 10:26:48 +0530

    Graph UI Created

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
-<<<<<<< HEAD
-import Sustainability from './components/Sustainability'
 import Blog from './components/Blog';
-=======
+
 import CO2Savings from './components/CO2Savings/CO2Dashboard'
 import Team from './components/Team'
->>>>>>> e45291d (Graph UI built)
+
 import Contact from './components/Contact'
 import GetQuote from './components/GetQuote'
 
@@ -24,7 +22,6 @@ function App() {
    <About/>
    <Services/>
    <GetQuote/>
-   <Sustainability/>
    <Blog/>
    <CO2Savings/>
    <Team/>
