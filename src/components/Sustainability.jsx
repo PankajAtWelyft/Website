248344415f677e0a6a857e48baff0145ec87f46d
guptamayank9 <guptamayank242032@gmail.com>
2026-06-02 10:11:07 +0530

    Added new updates

--- src/components/Sustainability.jsx
+++ src/components/Sustainability.jsx
@@ -46,42 +46,6 @@ const Sustainability = () => {
           />
         </div>
       </div>
-      <div className="bg-[#F5F3EE] py-30">
-        <div className="max-w-7xl mx-auto flex items-center gap-32 px-10">
-          <div className="w-1/2">
-            <p className="inline-block text-sm font-semibold uppercase tracking-widest bg-[#0A1F44] text-yellow-400 px-5 py-2 rounded-md mb-8">
-              Why Welyft
-            </p>
-            <h2 className="text-5xl text-[#0A1F44] font-medium">
-              Dependable, <span className="italic">on time</span>
-            </h2>
-            <hr className="w-30 border-4 rounded-xl border-yellow-400 my-8" />
-            <h3 className="text-[#0A1F44] text-4xl mt-8 italic font-medium ">
-              For High-Value Businesses
-            </h3>
-            <p className="text-xl  text-gray-600 leading-relaxed mt-7">
-              When your people depend on it, so do we. From onboarding day one
-              to long-term planning, Welyft delivers without a beat dropped — at
-              the scale and speed your business demands.
-            </p>
-            <div className="flex gap-6 mt-10">
-              <button className="px-6 py-3 font-semibold bg-amber-300 hover:scale-105 rounded-3xl text-xl hover:bg-indigo-900 hover:text-yellow-400  transition">
-                Get Demo →{" "}
-              </button>
-              <button className="px-8 py-3  font-semibold bg-[#fffefc] rounded-3xl hover:scale-105 text-xl hover:bg-indigo-900 hover:text-yellow-400  transition">
-                Our Story
-              </button>
-            </div>
-          </div>
-          <div className="w-1/2">
-            <img
-              src={serv}
-              className=" w-full rounded-3xl h-150 object-cover hover:scale-105 transition-all duration-500"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
     </>
   );
 };
