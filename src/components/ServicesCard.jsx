aa6a38d6193015272c36d657ef57652122b32c4e
guptamayank9 <guptamayank242032@gmail.com>
2026-06-03 13:22:51 +0530

    update changes in service card

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-const ServicesCard = ({ image, title, description, buttonTxt }) => {
+const ServicesCard = ({ image, title, description,}) => {
   return (
-      <div className="flex h-full flex-col overflow-hidden rounded-2xl bg-white shadow-md transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
+      <div className="flex h-full min-h-[500px] flex-col overflow-hidden rounded-2xl bg-white shadow-md transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl">
+
         <img src={image} className="h-52 w-full object-cover sm:h-56" alt="" />
+
         <div className="flex flex-1 flex-col p-5 pb-7 text-left sm:p-6 sm:pb-8">
+
           <h2 className="text-2xl font-semibold text-[#0A1F44] lg:text-3xl">
             {title}
           </h2>
-          <p className="mt-4 text-base leading-relaxed text-gray-600 sm:text-lg">
+          <p className="mt-4 text-base leading-relaxed text-gray-600 sm:text-lg flex-1">
             {description}
-          </p>
-         <button className="text-xl  text-left font-semibold text-[#0A1F44] mt-auto hover:text-yellow-500 transition-all duration-300"
-          >{buttonTxt}</button>           
+          </p>           
         </div>
       </div>
   );
