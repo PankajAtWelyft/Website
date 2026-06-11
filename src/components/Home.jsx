b70cd6ec1b6dd1a57178727c985285c8ccf3b6ff
shyamsunder98929 <shyamsuthar9829@gmail.com>
2026-06-11 07:53:49 +0530

    Mobile banners added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,20 +8,24 @@ import Hero_page from "../assets/welyft-home-banner-img.png";
 import B2B_banner from "../assets/welyft-B2B-banner-img.png";
 import C2C_banner from "../assets/welyft-C2C-banner-img.png";
 import Logistics_Technology from "../assets/welyft-logistics+technology-img.png";
+import MobileHero_page from "../assets/welyft-mobile-home-banner.png";
+import MobileB2B_banner from "../assets/welyft-mobile-B2B-banner.png";
+import MobileC2C_banner from "../assets/Welyft-mobile-C2C-banner.png";
+import MobileLogistics_Technology from "../assets/welyft-mobile-logistics+technology.png";
 
 const homeSlides = [
-  { image: Hero_page, alt: "Welyft hero banner" },
-  { image: B2B_banner, alt: "Welyft B2B banner" },
-  { image: C2C_banner, alt: "Welyft C2C banner" },
-  { image: Logistics_Technology, alt: "Logistics and technology image" },
+  { image: Hero_page, mobileImage: MobileHero_page, alt: "Welyft hero banner" },
+  { image: B2B_banner, mobileImage: MobileB2B_banner, alt: "Welyft B2B banner" },
+  { image: C2C_banner, mobileImage: MobileC2C_banner, alt: "Welyft C2C banner" },
+  { image: Logistics_Technology, mobileImage: MobileLogistics_Technology, alt: "Logistics and technology image" },
 ];
 
 const Home = () => {
   const swiperRef = React.useRef(null);
 
   return (
-    <section id="home" className="w-full pt-17">
-      <div className="relative mx-auto w-full overflow-hidden rounded-none bg-[#F5F3EE]">
+    <section id="home" className="w-full pt-15 sm:pt-8 lg:pt-17">
+      <div className="relative  w-full overflow-hidden rounded-none bg-[#F5F3EE]">
         <Swiper
           onSwiper={(swiper) => {
             swiperRef.current = swiper;
@@ -37,13 +41,16 @@ const Home = () => {
         >
           {homeSlides.map((slide) => (
             <SwiperSlide key={slide.alt} className="bg-transparent">
-              <div className="flex h-[28vh] max-h-[56rem] items-center justify-center overflow-hidden bg-transparent sm:h-[38vh] lg:h-[92vh]">
-                <img
-                  src={slide.image}
-                  alt={slide.alt}
-                  className="h-full w-full object-cover"
-                  loading="eager"
-                />
+              <div className="flex h-[70vh] items-center justify-center overflow-hidden bg-transparent sm:h-[110vh] lg:h-[92vh]">
+                <picture className="block h-full w-full">
+                  <source media="(max-width: 1024px)" srcSet={slide.mobileImage} />
+                  <img
+                    src={slide.image}
+                    alt={slide.alt}
+                    className="h-full w-full object-contain"
+                    loading="eager"
+                  />
+                </picture>
               </div>
             </SwiperSlide>
           ))}
